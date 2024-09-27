da99c610710f26eda7fbc0fe65de915c007ac58f
Brandon Hamilton <bham2015@gmail.com>
2024-09-27 18:39:25 -0500

    Set up functionality for the form, next step is testing.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,19 +1,55 @@
 import { useState } from "react";
 
+const API_URL = `https://fsa-jwt-practice.herokuapp.com/signup`;
+
 export default function SignUpForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "",
+          password: "",
+        }),
+      });
+      const responseObj = await response.json();
+      console.log(responseObj);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
     <>
       <h2>Sign Up!</h2>
-      <form>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
         <label>
-          Username<input></input>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          ></input>
         </label>
         <label>
-          Password<input></input>
+          Password
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
         </label>
         <button>Submit</button>
       </form>
