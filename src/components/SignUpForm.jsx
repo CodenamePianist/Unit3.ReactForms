ac0e1e389f926e3b03b4ec7a44ec8ed73e60c64d
Brandon Hamilton <bham2015@gmail.com>
2024-09-27 17:48:46 -0500

    Added a form to the SignUpForm component.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+
 export default function SignUpForm() {
-  return <h2>Sign Up!</h2>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      <form>
+        <label>
+          Username<input></input>
+        </label>
+        <label>
+          Password<input></input>
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
